d6c75556315feaad731ecee27d01c1256bed3eca
Medinah Subair <subairmedinah@gmail.com>
2022-09-11 23:18:42 +0000

    combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - print combination of two digit numbers
+ *
+ * Return: 0 to exit program
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = '0'; a <= '8'; a++)
+	{
+		for (b = '1'; b <= '9'; b++)
+		{
+			if (a != b && b != a)
+			{
+				if (b > a)
+				{
+					putchar(a);
+					putchar(b);
+					if (!(a == '8' && b == '9'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
