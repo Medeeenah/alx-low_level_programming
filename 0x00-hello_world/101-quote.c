0d7b1ba5f4866f4c82aeeae4bf6de985e270b557
Medinah Subair <subairmedinah@gmail.com>
2022-09-11 14:59:52 +0000

    standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - test unix simplicity
+ * main - printing to standard error
  *
  * Return: 1 to the end of the program
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59, 1, stderr);
         return (1);
 }
