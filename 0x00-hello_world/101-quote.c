447d909ecdd6a73f40a5e308fbd8255c263cacd2
Medinah Subair <subairmedinah@gmail.com>
2022-09-11 14:36:14 +0000

    simple unix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - test unix simplicity
+ *
+ * Return: 1 to the end of the program
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 57);
+        return (1);
+}
