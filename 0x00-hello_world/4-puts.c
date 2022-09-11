cffc27e6d780b6343c3d14c5eba2d158c33280c2
Medinah Subair <subairmedinah@gmail.com>
2022-09-11 11:00:14 +0000

    hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints texts with put
+ *   
+ * Return: 0 to end the program
+ */
+int main(void)
+{ 
+	char text[] = *\"Programming is like building a multilingual puzzle";
+
+	puts(text);
+	return (0);
+}
