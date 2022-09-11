b461c70bdb25640513e9b89687f4f8e817fc7943
Medinah Subair <subairmedinah@gmail.com>
2022-09-11 22:54:02 +0000

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print all numbers of base 16
+ *
+ * Return: 0 to exit program
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+
+	{
+		putchar(i);
+	}
+	for (i = 0; i < 6; i++)
+	{
+		putchar(i + 'a');
+	}
+	putchar('\n');
+	return (0);
+}
