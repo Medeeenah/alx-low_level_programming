c41aae8ce9549a946516b1fb295772e5e7b28135
Medinah Subair <subairmedinah@gmail.com>
2022-09-13 18:55:32 +0000

    lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabet code
+ *
+ * Return: void.
+ */
+void print_alphabet(void)
+
+{
+	int i;
+	for (i = 'a'; i <= 'z'; i++)
+
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
