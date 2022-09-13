7093a7e0b6bb82f4ffac46051f9c2431816fa4fb
Medinah Subair <subairmedinah@gmail.com>
2022-09-13 18:32:50 +0000

    -putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Prints _putchar
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+	_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
