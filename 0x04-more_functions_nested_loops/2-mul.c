7c559bb52a17c649bd993b44121a6f05f9d387b0
Medinah Subair <subairmedinah@gmail.com>
2022-09-15 22:07:46 +0000

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ * mul- multiplies two integers
+ * @a: first variable for multiplication
+ * @b: second variable for multiplication
+ *
+ * Return: value of multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
