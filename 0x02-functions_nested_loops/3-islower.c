08890896b09857783e5d253e0df2b70e7ad59771
Medinah Subair <subairmedinah@gmail.com>
2022-09-15 21:04:25 +0000

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+/**
+ * _islower - checks if argument is lower case
+ * @c: variable to check if lower case
+ *
+ * Return: 1 if lower case otherwise 0
+ */
+int _islower(int c)
+{
+
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
