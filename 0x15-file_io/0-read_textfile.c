87de91489a767923442c74bcb5009ce8c6df4bb9
Medinah Subair <subairmedinah@gmail.com>
2022-10-31 19:28:56 +0100

    read

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,7 +7,7 @@
  *
  * Return: number of characters printed
  */
-size_t read_textfile(const char *filename, size_t letters)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int myfile, i, check;
 	ssize_t count;
